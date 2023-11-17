e26b893ae95b8558e02f68fc93465f1a600ce901
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-11-17 06:01:45 -0800

    all set

--- built_emu.c
+++ built_emu.c
@@ -56,49 +56,44 @@ return (0);
  */
 int _currentdr(info_t *info)
 {
-char *s, *dir, buffer[1024];
-int chdir_ret;
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-s = getcwd(buffer, 1024);
-
-if (!s)
-_puts("TODO: >>getcwd failure emsg here<<\n");
-
-if (!info->argv[1])
-{
-dir = get_env(info, "HOME=");
-
-if (!dir)
-chdir_ret = /* TODO: what should this be? */
-chdir((dir = get_env(info, "PWD=")) ? dir : "/");
-else
-chdir_ret = chdir(dir);
-}
-else if (_strcmp(info->argv[1], "-") == 0)
-{
-if (!get_env(info, "OLDPWD="))
-{
-_puts(s);
-_putchar('\n');
-return (1);
-}
-_puts(get_env(info, "OLDPWD=")), _putchar('\n');
-chdir_ret = /* TODO: what should this be? */
-chdir((dir = get_env(info, "OLDPWD=")) ? dir : "/");
-}
-else
-chdir_ret = chdir(info->argv[1]);
-
-if (chdir_ret == -1)
-{
-_error(info, "can't cd to ");
-_eputs(info->argv[1]), _eputchar('\n');
-}
-else
-{
-_nwsenv(info, "OLDPWD", get_env(info, "PWD="));
-_nwsenv(info, "PWD", getcwd(buffer, 1024));
-}
-
-return (0);
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = get_env(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = get_env(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!get_env(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(get_env(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = get_env(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_nwsenv(info, "OLDPWD", get_env(info, "PWD="));
+		_nwsenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
--- hx_function.c
+++ hx_function.c
@@ -7,21 +7,21 @@
  */
 char *gets_history_file(info_t *info)
 {
-char *buf, *dir;
+	char *buf, *dir;
 
-dir = get_env(info, "HOME=");
+	dir = get_env(info, "HOME=");
 
-if (!dir)
-return (NULL);
-buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
 
-if (!buf)
-return (NULL);
-buf[0] = 0;
-_strcpy(buf, dir);
-_strcat(buf, "/");
-_strcat(buf, HIST_FILE);
-return (buf);
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
 }
 
 /**
@@ -31,52 +31,45 @@ return (buf);
  */
 int read_file_history(info_t *info)
 {
-int i, last = 0, linecount = 0;
-ssize_t fd, rdlen, fsize = 0;
-struct stat st;
-char *buf = NULL, *filename = get_thehistory_file(info);
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_thehistory_file(info);
 
-if (!filename)
-return (0);
-fd = open(filename, O_RDONLY);
-free(filename);
-
-if (fd == -1)
-return (0);
-
-if (!fstat(fd, &st))
-fsize = st.st_size;
-
-if (fsize < 2)
-return (0);
-buf = malloc(sizeof(char) * (fsize + 1));
-
-if (!buf)
-return (0);
-rdlen = read(fd, buf, fsize);
-buf[fsize] = 0;
-
-if (rdlen <= 0)
-return (free(buf), 0);
-close(fd);
-
-for (i = 0; i < fsize; i++)
-if (buf[i] == '\n')
-{
-buf[i] = 0;
-build_history_link(info, buf + last, linecount++);
-last = i + 1;
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_link(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_link(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
 }
-if (last != i)
-build_history_link(info, buf + last, linecount++);
-free(buf);
-info->histcount = linecount;
-while (info->histcount-- >= HIST_MAX)
-delete_node_at_index(&(info->history), 0);
-renumber_history(info);
-return (info->histcount);
-}
-
 /**
  * create_history - creates a file, or appends to an existing file
  * @info: the parameter struct
